5db9ffe96f2f3bf1f64a2378a807aca9aa5cbb6b
wyxmg <ejs.important@gmail.com>
2017-01-08 11:20:39 -0500

    h=13 fixes

--- ardcalbot.js
+++ ardcalbot.js
@@ -5,20 +5,56 @@ var versionNum = 1.0;
 var eventText = [];
 
 
-//var port_number = server.listen(process.env.PORT || 3000);
-//app.listen(port_number);
 
-//var http = require('http'); 
-//http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
-/*
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
-*/
+ var http = require('http');
+ var fs = require('fs');
+ var path = require('path');
 
-var fs = require('fs');
+ http.createServer(function (request, response) {
+
+    console.log('request starting for ');
+    console.log(request);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    path.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+
+ }).listen(process.env.PORT || 5000);
+
+ console.log('Server running at http://127.0.0.1:5000/');
+
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
